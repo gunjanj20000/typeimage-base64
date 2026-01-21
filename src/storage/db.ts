f227ed93479aed286af919b121e852750bf780e0
Gunjan jangid <jangidgunjan08@gmail.com>
2026-01-21 11:46:19 +0530

    Delete src directory

--- src/storage/db.ts
+++
@@ -1,9 +0,0 @@
-import { openDB } from "idb";
-
-export const imageDB = openDB("typeimage-files", 1, {
-  upgrade(db) {
-    if (!db.objectStoreNames.contains("images")) {
-      db.createObjectStore("images");
-    }
-  },
-});
